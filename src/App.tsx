04ab199fc0de54d0946d0015bce00b03e4141e9f
phillipdunn <phillipdunn@hotmail.com>
2024-08-22 20:45:15 +0100

    feat: basic setup with mui, react router dom, images and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Quiz from './pages/Quiz';
+import Results from './pages/Results';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/quiz' element={<Quiz />} />
+      <Route path='/results' element={<Results />} />
+      <Route path='*' element={<div>Not Found</div>} />
+    </Routes>
   );
 }
 
